ca94605b09264510ffa0f6c1ada0581a9fd238b8
fau311 <syifafau311@gmail.com>
2026-04-15 09:14:06 +0700

    Add banyak_langkah function for step counting
    Implement recursive function to count steps for a number.

--- soal2.c
+++ soal2.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+int banyak_langkah(int n){
+    // Jika n merupakan angka 1, maka keluar dari fungsi 
+    if (n == 1){
+        return 0;
+    }
+    // Jika n bilangan genap
+    if (n % 2 == 0){
+        return 1 + banyak_langkah(n/2);
+    }
+    // Jika n selain bilangan genap dan lebih besar dari 1
+    else if(n % 2 != 0 && n > 1){
+        return 1 + banyak_langkah(3*n + 1);
+    }
+}
 
-int main(void) {
+int main(){
+    int jumlah, langkah, hasil, n;
+    scanf("%d", &jumlah);
+    for(int i = 0; i < jumlah; i++){
+        scanf("%d", &n);
+        hasil = banyak_langkah(n);
+        printf("LANGKAH %d\n", hasil);
+    }
     return 0;
 }
+
+// source code : https://www.w3schools.com/c/c_functions_recursion.php
